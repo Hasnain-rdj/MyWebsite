87810652424c484788b708f2ba33911d272da529
Hasnain N. <mhussnainzardari34@gmail.com>
2025-02-05 23:06:04 +0500

    Stark Industries Log In Form

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("logIn").addEventListener("click", function () {
+    let username = document.getElementById("uName").value;
+    let password = document.getElementById("pass").value;
+
+    if (username === "Hasnain" && password === "cfd@3718") {
+        window.location.href = "https://22f-3718.github.io/WebEngineering_Website_Hosting/";
+    } else {
+        alert("Invalid Username or Password");
+    }
+});
+
+document.getElementById("clearit").addEventListener("click", function () {
+    username = document.getElementById("uName").value = "";
+    password = document.getElementById("pass").value = "";
+});
