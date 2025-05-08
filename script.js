158e31af58bf74c483b9c8f7ae4c70bac096136f
Hasnain N. <mhussnainzardari34@gmail.com>
2025-05-09 01:28:20 +0500

    Changes into the main

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("logIn").addEventListener("click", function () {
     let username = document.getElementById("uName").value;
     let password = document.getElementById("pass").value;
 
-    if (username === "Hasnain" && password === "cfd@3718") {
+    if (username === "admin" && password === "admin") {
         window.location.href = "https://hasnain-rdj.github.io/Portfolio/";
     } else {
         alert("Invalid Username or Password");
